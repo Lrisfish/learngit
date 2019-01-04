e94ef6c8abc006a55872b7b089ffddf7478e9d07
Lily <782743461@qq.com>
2019-01-04 17:03:44 +0800

    feature1 add

--- test.cpp
+++ test.cpp
@@ -7,8 +7,6 @@ int main(){
 	cout << "master分支" << endl;
 	cout << "dev分支添加任务" << endl;
 	cout << "feature1分支添加功能" << endl;
-	cout << "feature1 add" << endl;
-	cout << "dev add" << endl;
 	return 0;
 
 }
